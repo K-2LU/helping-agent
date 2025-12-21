2245e8ddf9487aae705e4ee48ad6bea8efb09dc0
K-2LU <rigansust@gmail.com>
2025-12-21 18:53:14 +0600

    feat: SessionController security fix

--- app/controller/SessionController.ts
+++ app/controller/SessionController.ts
@@ -1,21 +1,10 @@
 import User from "#models/user";
-import hash from "@adonisjs/core/services/hash";
 import type { HttpContext } from "@adonisjs/core/http";
 
 export default class SessionController {
-    async store({request, response} : HttpContext)  {
+    async store({request } : HttpContext)  {
         const { phoneNumber, password } = request.only(['phoneNumber', 'password']);
 
-        const user = await User.findBy('phoneNumber', phoneNumber);
-
-        if(!user)   {
-            return response.abort('Invalid credentials');
-        }
-
-        const isPasswordValid = await hash.verify(user.password, password)
-
-        if(!isPasswordValid)    {
-            return response.abort('Invalid Credentials')
-        }
+        const user = await User.verifyCredentials(phoneNumber, password);
     }
 }
