a65801b2ab026e142175ebbb905270a9e6f54824
K-2LU <rigansust@gmail.com>
2025-12-22 16:53:37 +0600

    feat: model for otp

---
+++ app/models/otp.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations';
+import User from './user.js';
+
+export default class Otp extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+  
+  @column()
+  declare otp: string
+
+  @column()
+  declare userId: number;
+
+  @column()
+  declare purpose: 'verification' | 'reset'
+
+  @column()
+  declare expiresAt: DateTime
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+}
