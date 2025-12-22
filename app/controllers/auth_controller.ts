d5bb1ba8577dcbaec801085fd740a1d7adf03662
K-2LU <rigansust@gmail.com>
2025-12-22 10:40:02 +0600

    feat: init and create login_controller

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,35 @@
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
+import hash from '@adonisjs/core/services/hash';
+
+export default class AuthController {
+    async signup({ request, response }: HttpContext) {
+        const data = request.only([
+            'name', 'phoneNumber', 'password', 'location'
+        ]);
+
+        const exists = await User.query()
+            .where('phone_number', data.phoneNumber)
+            .first();
+
+        if (exists) {
+            return response.conflict({message: 'entry already exists'})
+        }
+
+        const user = await User.create({
+            name: data.name,
+            phoneNumber: data.phoneNumber,
+            location: data.location,
+            password: await hash.make(data.password),
+            verified: false
+        })
+
+        const token = await User.accessTokens.create(user)
+
+        return response.created({
+            type: 'bearer ',
+            token: token.value?.release(),
+            user,
+        })
+    }
+}
