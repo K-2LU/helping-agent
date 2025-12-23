3c7ec4832b1b625c0e4b2ab9d3cf135edb44d33a
K-2LU <rigansust@gmail.com>
2025-12-23 13:24:34 +0600

    del: unused imports

--- start/routes.ts
+++ start/routes.ts
@@ -6,9 +6,5 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
-import router from '@adonisjs/core/services/router'
-import UsersController from '#controllers/users_controller'
-
 import './routes/v1/auth_routes.js'
 import './routes/v1/user_routes.js'
