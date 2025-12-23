02b7f485e6bd84cb519054f9a9b592c1f635683f
K-2LU <rigansust@gmail.com>
2025-12-23 16:49:28 +0600

    feat: fallback to default value

--- app/validators/paginate.ts
+++ app/validators/paginate.ts
@@ -6,3 +6,18 @@ export const paginationValidator = vine.compile(
         limit: vine.number().min(1).max(50).optional(),
     })
 )
+
+export const paginationValidatorWithDefault = vine.compile(
+    vine.object({
+        page: vine.number().optional().transform((value) => {
+            if (!value || value < 1) return 1;
+            return value;
+        }),
+        limit: vine.number().optional().transform((value) => {
+            if (!value) return 10;
+            if (value > 50) return 50;
+            if (value < 1) return 10;
+            return value;
+        }),
+    })
+)
