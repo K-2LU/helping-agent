8b46b2d28458b0b84e67ae044e886f8074aac688
K-2LU <rigansust@gmail.com>
2025-12-23 11:45:09 +0600

    feat: getAll services

--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -1,3 +1,33 @@
+import User from "#models/user";
+import { UserType } from "#types/user";
+import db from "@adonisjs/lucid/services/db";
+
 export class UserService {
-  // Your code here
+  async getAll(): Promise<UserType[]> {
+    const dbUsers = await User.query()
+      .select('users.*')
+      .select(
+        db.from('occupation_histories')
+          .join('occupations', 'occupations.id', 'occupation_histories.occupation_id')
+          .whereColumn('occupation_histories.user_id', 'user.id')
+          .orderBy('occupation_histories.starting_date', 'desc')
+          .limit(1)
+          .select('occupations.name')
+          .as('active_occupation_name')
+      )
+
+      const publicUsers: UserType[] = dbUsers.map((user) => {
+        return {
+          data: {
+            name: user.name,
+            phoneNumber: user.phoneNumber,
+            location: user.location,
+            verified: !!user.verified,
+          },
+          occupation: user.$extras.active_occupation_name || null
+        }
+      })
+
+      return publicUsers;
+  }
 }
