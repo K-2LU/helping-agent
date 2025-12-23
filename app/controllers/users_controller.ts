0b3189a32772935151792ff075ce6d971f3fdfcc
K-2LU <rigansust@gmail.com>
2025-12-23 15:46:03 +0600

    feat: pagination validation

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -2,6 +2,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 import { UserService } from "#services/user_service";
 import { inject } from "@adonisjs/core";
+import { paginationValidator } from '#validators/paginate';
 
 @inject()
 export default class UsersController {
@@ -10,8 +11,10 @@ export default class UsersController {
     ) { }
 
     async getAll({ request, response }: HttpContext) {
-        const page = request.input('page', 1);
-        const limit = request.input('limit', 10);
+        const payload = await request.validateUsing(paginationValidator)
+
+        const page = payload.page || 1;
+        const limit = payload.limit || 10;
 
         const result = await this.userService.getAll(page, limit);
         
