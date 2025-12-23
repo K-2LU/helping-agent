bbd9f424b01e9485c731788a176f7b44db126410
K-2LU <rigansust@gmail.com>
2025-12-23 12:57:29 +0600

    del: unused '/' path

--- start/routes.ts
+++ start/routes.ts
@@ -10,12 +10,6 @@
 import router from '@adonisjs/core/services/router'
 import UsersController from '#controllers/users_controller'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
-
 import './routes/auth_routes.js'
 
 router.get('/get-all', [UsersController, 'getAll']);
