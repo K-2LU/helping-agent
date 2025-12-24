406e573f1e1c5737fec1d8164d29c61a37c6797c
K-2LU <rigansust@gmail.com>
2025-12-24 10:54:48 +0600

    feat: validator for occupationCreation controller

--- app/validators/occupation.ts
+++ app/validators/occupation.ts
@@ -4,6 +4,5 @@ export const CreateOccupationValidator = vine.compile(
     vine.object({
         name: vine.string().minLength(3).maxLength(20),
         details: vine.string().maxLength(255),
-        
     })
 )
