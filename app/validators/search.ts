7538c84ffcacab4f169695669753b9fe362ff96f
K-2LU <rigansust@gmail.com>
2025-12-24 15:52:43 +0600

    refactor: used shared pagination schema

--- app/validators/search.ts
+++ app/validators/search.ts
@@ -1,19 +1,13 @@
 import vine from '@vinejs/vine'
+import { paginationSchemaWithDefault } from './paginate.js'
 
 export const SearchParamsValidator = vine.compile(
     vine.object({
         searchString: vine.string().minLength(3).maxLength(12),
-        limit: vine.number().optional().transform((value) =>{
-            if(!value) return 3
-            if (value < 1)  return 3
-            if (value > 15) return 15
-            
-            return value
-        })
+        ...paginationSchemaWithDefault,
     })
 )
 
 export const searchParamsMessages = {
         'searchString.required': 'A search string is required',
-        'searchString.minLength': 'Search string must be at least 3 characters long',
 }
