fb33b26b673bdb77fa2dc5828bd5926f095e512e
K-2LU <rigansust@gmail.com>
2025-12-24 11:40:31 +0600

    feat: service for public view of occupations

--- app/services/occupation_service.ts
+++ app/services/occupation_service.ts
@@ -1,14 +1,39 @@
 import Occupation from "#models/occupation";
-import { CreateOccupationDTO } from "#types/occupation";
+import { PaginatedResult } from "#types/common";
+import { CreateOccupationDTO, PublicOccupationType } from "#types/occupation";
 
 export class OccupationService {
-  async create(payload: CreateOccupationDTO)    {
+  async create(payload: CreateOccupationDTO) {
     const query = await Occupation.create({
-        name: payload.name,
-        details: payload.details,
-        createdBy: payload.createdBy
+      name: payload.name,
+      details: payload.details,
+      createdBy: payload.createdBy
     })
 
     return query;
+  }
+
+  async get(page: number, limit: number)
+    : Promise<PaginatedResult<PublicOccupationType>> {
+
+    const occupationList = await Occupation.query()
+      .select('name', 'details')
+      .orderBy('created_at', 'desc')
+      .paginate(page, limit)
+
+    const serializedData = occupationList.serialize()
+
+    const publicOccupations: PublicOccupationType[] = occupationList.all()
+      .map((occupation) => {
+        return {
+          name: occupation.name,
+          details: occupation.details,
+        }
+      })
+
+    return {
+      meta: serializedData.meta,
+      data: publicOccupations
+    }
   }
 }
